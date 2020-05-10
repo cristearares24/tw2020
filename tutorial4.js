9e7e63d7b692c16f453148eda436466422104610
alinapopa06 <39854609+alinapopa06@users.noreply.github.com>
2020-05-10 18:50:55 +0300

    Add files via upload

---
+++ tutorial4.js
@@ -0,0 +1,47 @@
+let allRead = false;
+let inProgress = false;
+
+function myFunction(elem)
+{
+    document.getElementById(elem.id).style.background = "green";
+    inProgress = true;
+    localStorage.setItem('inProgress4', JSON.stringify(inProgress));
+    let btn = document.getElementsByClassName('btn');
+    let i;
+    localStorage.setItem('btn4'+elem.id, JSON.stringify(true));
+    for(i = 0; i < btn.length; i++)
+    {
+        if(btn[i].style.background != "green")
+        {
+            return;
+        }
+    }
+    localStorage.setItem('allRead4', JSON.stringify(inProgress));
+}
+
+function startQuiz(){
+    let allButtons = document.getElementsByClassName("btn");
+    let i;
+    for(i = 0; i < allButtons.length; i++)
+    {
+        if(allButtons[i].style.background != 'green')
+        {
+            alert('Trebuie sa cititi toti pasii inainte de a incepe quiz-ul');
+            return;
+        }
+    }
+    window.location.href = "quiz1.html";
+}
+
+window.onload = function(){
+    let prog = document.getElementsByClassName('btn')
+    let j;
+    for(j = 0; j < prog.length; j++)
+    {
+        let btn = JSON.parse(localStorage.getItem('btn4' + prog[j].id));
+        if(btn)
+        {
+            prog[j].style.background = 'green';
+        }
+    }
+}
