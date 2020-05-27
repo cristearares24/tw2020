ec4e6ea44ce4f4055c9ed7551f4fb366008d5934
alinapopa06 <39854609+alinapopa06@users.noreply.github.com>
2020-05-27 22:08:10 +0300

    Delete questions.js

--- questions.js
+++
@@ -1,7 +0,0 @@
-var questions = [
-  ["ABCDASDASD?", "asdasd", "asdasasd", "asdasdasd","asdasdas", "3"],
-  ["What is Cyclops’ power in X-Men?", "Telekinesis", "Optic blast", " Diamond skin", "all","2"],
-  ["What is the name of the cosmic cube that Loki takes control of in The Avengers?", "the Tesseract", "the Magic Cube", "the Mjolnir", "all","1"],
-  [" How many Avengers are there in the Avengers movie?", " 4", " 10", "6", "100","3"],
-  ["How do the Fantastic Four get their powers?", "  they were bitten by a radioactive spider", " they were exposed to cosmic radiation in space", " they were experimented on", "Dont know","2"],
-  ];
